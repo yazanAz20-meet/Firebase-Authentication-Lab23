2b3ed670d609bc2206e44bc5ccbf89e9a019f43e
yazanAz20-meet <yazan7azaizah@gmail.com>
2023-07-24 13:07:32 +0300

    still the bonus

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -15,7 +15,7 @@ Config = {
 }
 
 
-firebase = pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(Config)
 auth = firebase.auth()
 
 
@@ -26,12 +26,27 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     if request.method == "POST":
-        
+        email = request.form['email']
+        password = request.form['password']
+        try: 
+            login_session['user'] = auth.sign_in_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+        except:
+            print("error2")
+    
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        try: 
+            login_session['user'] = auth.create_user_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+        except:
+            print("error")
     return render_template("signup.html")
 
 
